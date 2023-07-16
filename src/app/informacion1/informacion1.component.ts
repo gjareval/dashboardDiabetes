20240d8d83e6bdcccdd41a5575de055fb0941e70
santi0ne <johsrodr@espol.edu.ec>
2023-07-16 18:25:26 -0500

    iconos

--- src/app/informacion1/informacion1.component.ts
+++ src/app/informacion1/informacion1.component.ts
@@ -15,22 +15,22 @@ export class Informacion1Component {
   constructor (){
 
     this.chart1 = {
-      title: '¿Qué es?',
+      title: '¿Qué es? ',
       content: 'La diabetes es una enfermedad crónica que afecta la forma en que el cuerpo procesa y utiliza la glucosa (azúcar) en la sangre.'
     };
 
     this.chart2 = {
-      title: 'Síntomas',
+      title: 'Síntomas ',
       content: 'Los síntomas de la diabetes incluyen sed excesiva y aumento de la frecuencia urinaria, junto con fatiga persistente.'
     };
 
     this.chart3 = {
-      title: 'Factores de riesgo',
+      title: 'Factores de riesgo ',
       content: 'Los factores de riesgo de la diabetes incluyen la obesidad, la inactividad física y antecedentes familiares de la enfermedad.'
     };
 
     this.chart4 = {
-      title: 'Tratamiento',
+      title: 'Tratamiento ',
       content: 'El tratamiento de la diabetes puede incluir cambios en el estilo de vida, como una dieta saludable y actividad física regular.'
     };
   }
--- src/app/informacion2/informacion2.component.ts
+++ src/app/informacion2/informacion2.component.ts
@@ -14,23 +14,23 @@ export class Informacion2Component {
 
   constructor(){
     this.chart1 = {
-      title: 'IMC',
+      title: 'IMC ',
       content: 'El IMC (Índice de Masa Corporal) es una medida que evalúa la relación entre el peso y la altura de una persona. Se calcula dividiendo el peso en kilogramos entre el cuadrado de la altura en metros. El IMC se utiliza como indicador general de sobrepeso, obesidad o bajo peso, pero no considera la composición corporal específica.'
     };
 
     this.chart2 = {
-      title: 'Glucosa',
-      content: 'La glucosa es un tipo de azúcar que se encuentra en los alimentos que consumimos y es la principal fuente de energía para el cuerpo. Es absorbida por el torrente sanguíneo después de la digestión de los carbohidratos y se utiliza como combustible por las células para realizar sus funciones. Los niveles de glucosa en la sangre son regulados por la hormona insulina.'
+      title: 'Glucosa ',
+      content: 'La glucosa es un tipo de azúcar que se encuentra en los alimentos que consumimos y es la principal fuente de energía para el cuerpo. Es absorbida por el torrente sanguíneo después de la digestión de los carbohidratos y se utiliza como combustible por las células para realizar sus funciones. Los niveles de glucosa en la sangre son regulados por la insulina.'
     };
 
     this.chart3 = {
-      title: 'Presión sanguínea',
-      content: 'La presión sanguínea es la fuerza ejercida por la sangre contra las paredes de las arterias cuando el corazón late y se relaja. Se expresa en dos valores: la presión sistólica (la presión máxima durante la contracción del corazón) y la presión diastólica (la presión mínima cuando el corazón está en reposo). Se mide en milímetros de mercurio (mmHg) y es importante para evaluar la salud cardiovascular.'
+      title: 'Presión sanguínea ',
+      content: 'La presión sanguínea es la fuerza ejercida por la sangre contra las paredes de las arterias cuando el corazón late y se relaja. Se expresa en dos valores: la presión sistólica (la presión máxima durante la contracción del corazón) y la presión diastólica (la presión mínima cuando el corazón está en reposo). Se mide en milímetros de mercurio (mmHg).'
     };
 
     this.chart4 = {
-      title: 'Insulina',
-      content: 'La insulina es una hormona producida por el páncreas que desempeña un papel crucial en el metabolismo de la glucosa. Permite que las células del cuerpo utilicen la glucosa como fuente de energía al facilitar su absorción y almacenamiento. La insulina también ayuda a regular los niveles de glucosa en la sangre al disminuir su concentración cuando es demasiado alta. La falta de insulina o su utilización inadecuada puede dar lugar a la diabetes.'
+      title: 'Insulina ',
+      content: 'La insulina es una hormona producida por el páncreas que desempeña un papel crucial en el metabolismo de la glucosa. Permite que las células del cuerpo utilicen la glucosa como fuente de energía al facilitar su absorción y almacenamiento. La insulina también ayuda a regular los niveles de glucosa en la sangre. La falta de insulina puede dar lugar a la diabetes.'
     };
   }
 }
